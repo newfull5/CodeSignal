ca6ed599ae0c3695b3356b212b9a04b16d57002c
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-03 16:05:48 +0900

    Create Knapsack Light.py

---
+++ Knapsack Light.py
@@ -0,0 +1,13 @@
+        return value1 + value2
+def knapsackLight(value1, weight1, value2, weight2, maxW):
+    if weight1 + weight2 <= maxW:
+    elif min(weight1, weight2) > maxW:
+        return 0
+    else:
+        if weight1 <= maxW and weight2 > maxW:
+            return value1
+        if weight1 > maxW and weight2 <= maxW:
+        if weight1 <= maxW and weight2 <= maxW:
+            return max(value1, value2)
+            return value2
+            
